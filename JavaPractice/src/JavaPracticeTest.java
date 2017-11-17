6048c307aa55fd40bdf5b63ab94668c862c53498
Direk Sareewong <D.Sareewong@dazstory.com>
2017-11-17 16:51:51 +0100

    Add the test class for JavaPractice

---
+++ JavaPractice/src/JavaPracticeTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class JavaPracticeTest {
+
+	//Uploading even this, just to test Git
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
