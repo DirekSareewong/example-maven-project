2e41757008512c5b480ce592038d109daff88fe6
Direk Sareewong <D.Sareewong@dazstory.com>
2017-11-17 16:56:16 +0100

    Update the test on java practice

--- JavaPractice/src/test/JavaPracticeTest.java
+++ JavaPractice/src/test/JavaPracticeTest.java
@@ -5,7 +5,8 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class JavaPracticeTest {
-
+		
+	//Upload even this just to test git
 	@Test
 	public void test() {
 		fail("Not yet implemented");
